aae55ca6944f95199f4b24d15d1cf703da9727b9
tuipik <tuipiku@gmail.com>
2019-09-30 19:35:02 +0300

    rewrited on classes

--- collage.py
+++ collage.py
@@ -3,37 +3,53 @@ from PIL import Image
 from math import sqrt, ceil
 
 
-def create_collage(list_of_images, repo):
-    collage_width_hight = 800
-    cols_rows = int(ceil(sqrt(len(list_of_images))))
-    thumbnail_width_height = collage_width_hight//cols_rows
-    collage = Image.new('RGB', (collage_width_hight, collage_width_hight),
-                        (255, 255, 255))
+class Collage:
+    def __init__(self, list_of_images, repo):
+        self.list_of_images = list_of_images
+        self.repo = repo
+        self.collage_width_hight = 800
+        self.cols_rows = int(ceil(sqrt(len(self.list_of_images))))
+        self.thumbnail_width_height = self.collage_width_hight // self.cols_rows
+        self.collage = Image.new('RGB', (self.collage_width_hight,
+                                         self.collage_width_hight),
+                                 (255, 255, 255))
+        self.i = 0
+        self.x = 0
+        self.y = 0
+        self.thumbnail_images = []
 
-    thumbnail_images = []
-    for p in list_of_images:
-        image = Image.open(p)
-        if image.size < (thumbnail_width_height, thumbnail_width_height):
-            image = image.resize((thumbnail_width_height, thumbnail_width_height))
-        image.thumbnail((thumbnail_width_height, thumbnail_width_height))
-        thumbnail_images.append(image)
-    i = 0
-    x = 0
-    y = 0
-    try:
-        for col in range(cols_rows):
-            for row in range(cols_rows):
-                collage.paste(thumbnail_images[i], (x, y))
-                i += 1
-                x += thumbnail_width_height
-            y += thumbnail_width_height
-            x = 0
-    except IndexError:
-        pass
-    collage.save(f"{repo}/Collage.jpg")
+    def make_thumbnails(self, images, repo):
+        for p in self.list_of_images:
+            image = Image.open(p)
+            if image.size < (self.thumbnail_width_height,
+                             self.thumbnail_width_height):
+                image = image.resize((self.thumbnail_width_height,
+                                      self.thumbnail_width_height))
+            image.thumbnail((self.thumbnail_width_height,
+                             self.thumbnail_width_height))
+            self.thumbnail_images.append(image)
+        return self.thumbnail_images
+
+    def make_collage(self, thumbnails):
+        try:
+            for col in range(self.cols_rows):
+                for row in range(self.cols_rows):
+                    self.collage.paste(self.thumbnail_images[self.i],
+                                       (self.x, self.y))
+                    self.i += 1
+                    self.x += self.thumbnail_width_height
+                self.y += self.thumbnail_width_height
+                self.x = 0
+        except IndexError:
+            pass
+        self.collage.save(f"{self.repo}/Collage.jpg")
+
+    def run(self):
+        self.make_collage(self.make_thumbnails(self.list_of_images, self.repo))
 
 
-list_of_repos = [f'repos/{i}' for i in os.listdir(path="repos")]
-for repo in list_of_repos:
-    list_of_images = [f'{repo}/{i}' for i in os.listdir(path=repo)]
-    create_collage(list_of_images, repo)
+if __name__ == '__main__':
+    list_of_repos = [f'repos/{i}' for i in os.listdir(path="repos")]
+    for repo in list_of_repos:
+        list_of_images = [f'{repo}/{i}' for i in os.listdir(path=repo)]
+        Collage(list_of_images, repo).run()
--- main.py
+++ main.py
@@ -1,39 +1,66 @@
 import requests
 import os
 import config
+import collage_oop as col
 
 
-def download_avatars(search, auth=config.auth):
-    r = requests.get(f'https://api.github.com/search/repositories?q={search}',
-                     auth=auth)
-    answer = r.json()
-    repos_names = []
-    if len(answer['items']) >= 10:
-        for e, i in enumerate(answer['items'][:10]):
-            repos_names.append({f"{e+1}_{i['name']}": i['contributors_url']})
-    else:
-        for e, i in enumerate(answer['items'][:len(answer['items'])]):
-            repos_names.append({f"{e+1}_{i['name']}": i['contributors_url']})
+class AvatarsDownloader:
+    def __init__(self, search, auth=config.auth):
+        self.search = search
+        self.api_url = 'https://api.github.com/search/repositories?q='
+        self.auth = auth
+        self.r = requests.get(f'{self.api_url}{search}',
+                              auth=self.auth)
+        self.answer = self.r.json()
+        self.repos_names = []
 
-    for repo in repos_names:
-        for key in repo:
-            contributors = requests.get(repo[key], auth=auth)
+    def make_repo_dir(self):
+        if len(self.answer['items']) >= 10:
+            for e, i in enumerate(self.answer['items'][:10]):
+                self.repos_names.append({f"{e+1}_{i['name']}":
+                                             i['contributors_url']})
+        else:
+            for e, i in enumerate(self.answer['items']
+                                  [:len(self.answer['items'])]):
+                self.repos_names.append({f"{e+1}_{i['name']}":
+                                             i['contributors_url']})
 
-            if contributors.status_code == 200:
-                if not os.path.exists(f'repos/{key}'):
-                    os.makedirs(f'repos/{key}')
-                avatar_links = [i['avatar_url'] for i in contributors.json()]  # collects avatar urls from repo contributors
-                counter = 1
-                for link in avatar_links:
-                    avatar = requests.get(link)
-                    end = avatar.headers['Content-Type'].split('/')[-1]        # gets image type
+    def get_avatars_links(self, contributors, repo_name):
+        if contributors.status_code == 200:
+            if not os.path.exists(f'repos/{repo_name}'):
+                os.makedirs(f'repos/{repo_name}')
 
-                    with open(f"repos/{key}/avatar{counter}.{end}", 'wb') as f:
-                        f.write(avatar.content)
-                    counter += 1
-            else:
-                print(f'{key} repository has no contributors')
+            # collects avatar urls from repo contributors
+            avatar_links = [i['avatar_url'] for i in contributors.json()]
+            return avatar_links
+        else:
+            print(f'{repo_name} repository has no contributors')
+
+    def avatar_downloader(self, avatar_links, repo_name):
+        counter = 1
+        for link in avatar_links:
+            avatar = requests.get(link)
+
+            # gets image type
+            end = avatar.headers['Content-Type'].split('/')[-1]
+
+            with open(f"repos/{repo_name}/avatar{counter}.{end}", 'wb') as f:
+                f.write(avatar.content)
+            counter += 1
+
+    def run(self):
+        self.make_repo_dir()
+        for repo in self.repos_names:
+            for repo_name in repo:
+                contributors = requests.get(repo[repo_name], auth=self.auth)
+                self.avatar_downloader(self.get_avatars_links(contributors,
+                                                              repo_name),
+                                       repo_name)
 
 
 if __name__ == '__main__':
-    download_avatars(config.search)
+    AvatarsDownloader(config.search).run()
+    list_of_repos = [f'repos/{i}' for i in os.listdir(path="repos")]
+    for repo in list_of_repos:
+        list_of_images = [f'{repo}/{i}' for i in os.listdir(path=repo)]
+        col.Collage(list_of_images, repo).run()
