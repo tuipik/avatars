5decad04607c2c091e93d8ca592fc6d2e0ad3c8f
tuipik <tuipiku@gmail.com>
2019-09-30 22:47:46 +0300

    refactored code

--- main.py
+++ main.py
@@ -14,16 +14,18 @@ class AvatarsDownloader:
         self.answer = self.r.json()
         self.repos_names = []
 
-    def make_repo_dir(self):
-        if len(self.answer['items']) >= 10:
-            for e, i in enumerate(self.answer['items'][:10]):
+    def collect_repo_dirs(self):
+        items = self.answer['items']
+        if len(items) < 10:
+            for e, i in enumerate(items[:len(items)]):
                 self.repos_names.append({f"{e+1}_{i['name']}":
                                              i['contributors_url']})
-        else:
-            for e, i in enumerate(self.answer['items']
-                                  [:len(self.answer['items'])]):
-                self.repos_names.append({f"{e+1}_{i['name']}":
+            return len(items)
+
+        for e, i in enumerate(items[:10]):
+            self.repos_names.append({f"{e+1}_{i['name']}":
                                              i['contributors_url']})
+        return len(items)
 
     def get_avatars_links(self, contributors, repo_name):
         if contributors.status_code == 200:
@@ -32,30 +34,25 @@ class AvatarsDownloader:
 
             # collects avatar urls from repo contributors
             avatar_links = [i['avatar_url'] for i in contributors.json()]
-            return avatar_links
-        else:
-            print(f'{repo_name} repository has no contributors')
+
+            self.avatar_downloader(avatar_links, repo_name)
 
     def avatar_downloader(self, avatar_links, repo_name):
-        counter = 1
-        for link in avatar_links:
+        for num, link in enumerate(avatar_links):
             avatar = requests.get(link)
 
             # gets image type
             end = avatar.headers['Content-Type'].split('/')[-1]
 
-            with open(f"repos/{repo_name}/avatar{counter}.{end}", 'wb') as f:
+            with open(f"repos/{repo_name}/avatar{num+1}.{end}", 'wb') as f:
                 f.write(avatar.content)
-            counter += 1
 
     def run(self):
-        self.make_repo_dir()
-        for repo in self.repos_names:
-            for repo_name in repo:
-                contributors = requests.get(repo[repo_name], auth=self.auth)
-                self.avatar_downloader(self.get_avatars_links(contributors,
-                                                              repo_name),
-                                       repo_name)
+        self.collect_repo_dirs()
+        for repository in self.repos_names:
+            for repo_name in repository:
+                contributors = requests.get(repository[repo_name], auth=self.auth)
+                self.get_avatars_links(contributors, repo_name)
 
 
 if __name__ == '__main__':
